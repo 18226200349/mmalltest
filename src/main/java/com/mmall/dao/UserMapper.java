14513e20d35e43e8babff48c8d78b7b73a506c45
18226200349 <2023576033@qq.com>
2018-04-11 22:55:45 +0800

    user 数据持久层

--- src/main/java/com/mmall/dao/UserMapper.java
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -1,5 +1,7 @@
 package com.mmall.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.mmall.pojo.User;
 
 public interface UserMapper {
@@ -14,4 +16,8 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+    
+    int checkUsername(String username);
+    
+    User selectLogin(@Param("username")String username,@Param("password")String password);
 }
